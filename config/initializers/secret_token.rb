049199ba3069904e34bbeedb78e07095359bb15d
dancingshell <kaitlinjaffe@gmail.com>
2014-09-10 09:51:54 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MixnMatch::Application.config.secret_key_base = 'feddf940bd99715f61e7398091472b69c84b714e1e6c8c07f9cdf22b39140b5a752a179d8e4ce4d60b8387474c510be850621d5b1dc3b5e01f3a8688da3b274b'
