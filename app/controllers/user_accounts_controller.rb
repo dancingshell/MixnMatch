6bd67b2ab0eae648473d9834e92fda1e5c0d589a
riachkyle <riachkyle@gmail.com>
2014-09-15 15:48:36 -0700

    rerouted if lastfm and rdio email dont exist in their respective systems

--- app/controllers/user_accounts_controller.rb
+++ app/controllers/user_accounts_controller.rb
@@ -7,23 +7,36 @@ class UserAccountsController < ApplicationController
       if @user_account.provider == 'rdio'
         client = RdioApi.new(consumer_key: ENV['RDIO_KEY'], consumer_secret: ENV['RDIO_SECRET'])
         @rdio = client.findUser(email: @user_account.email)
-        # Insert rdio user does not exist statement
-        @rdio_key = @rdio['key']
-        @rdio_rotation = client.getHeavyRotation(user: @rdio_key)
-        @rdio_rotation.each do |rdio|
-          get_artists(rdio['artist'], 'rdio')
+        if @rdio.nil?
+          @user_account.destroy
+          redirect_to :back
+        else
+          @rdio_key = @rdio['key']
+          @rdio_rotation = client.getHeavyRotation(user: @rdio_key)
+          @rdio_rotation.each do |rdio|
+            get_artists(rdio['artist'], 'rdio')
+          end
+          redirect_to root_path
         end
       end
 
       if @user_account.provider == 'lastfm'
-        url = HTTParty.get("http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=#{@user_account.username}&api_key=2d4eafcd13fe4bd7da79ea43ecd3dded&format=json")
+
+        url = HTTParty.get("http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=#{@user_account.username}&api_key=" + ENV['LASTFM_KEY'] + "&format=json")
         @lastfm = JSON.parse(url.body)
-        @lastfm['topartists']['artist'].each do |lastfm|
-          get_artists(lastfm['name'], 'lastfm')
+       
+        if @lastfm['error'] == 6
+          @user_account.destroy
+          redirect_to :back
+        else
+          @lastfm['topartists']['artist'].each do |lastfm|
+            get_artists(lastfm['name'], 'lastfm')
+          end
+          redirect_to root_path
         end
       end
 
-      redirect_to root_path
+      
     else
       # render
     end
