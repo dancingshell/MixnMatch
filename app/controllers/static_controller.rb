b56687aa90d8f163adc7a2123e1e4b921c4f35e2
brandonkwong <brandonjk@gmail.com>
2014-09-16 19:57:37 -0700

    Created redirect to signup if not current_user and added Facebook and login to page

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,13 +1,17 @@
 class StaticController < ApplicationController
 
   def index
-    if current_user && current_user.provider == "facebook"
-      url = HTTParty.get("https://graph.facebook.com/#{current_user.uid}/music?access_token=#{current_user.oauth_token}&method=GET&metadata=true&limit=1000&format=json")
-      @music = JSON.parse(url.body)
-      @music["data"].each do |band|
-        # call method from application controller to pull artists from a provider  
-        get_artists(band["name"], "facebook")
+    if current_user
+      if current_user && current_user.provider == "facebook"
+        url = HTTParty.get("https://graph.facebook.com/#{current_user.uid}/music?access_token=#{current_user.oauth_token}&method=GET&metadata=true&limit=1000&format=json")
+        @music = JSON.parse(url.body)
+        @music["data"].each do |band|
+          # call method from application controller to pull artists from a provider  
+          get_artists(band["name"], "facebook")
+        end
       end
+    else
+      redirect_to welcome_path
     end
   end
   
