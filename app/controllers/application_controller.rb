0704b2e9ad3c4592be4e8a8e2a699241d5eeb22e
brandonkwong <brandonjk@gmail.com>
2014-09-22 12:24:02 -0700

    Added EventWorker when new artists are imported, and cleaned up user_accounts controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,43 +39,4 @@ class ApplicationController < ActionController::Base
     UserArtist.create!(user: current_user, artist: artist, provider: provider) unless user_artist
   end
 
-  # def get_events(artist_name)
-
-  #   # Get Last.fm events from artists
-  #   url = HTTParty.get(
-  #     "http://ws.audioscrobbler.com/2.0/" +
-  #     "?method=artist.getevents" +
-  #     "&artist=#{url_encode(artist_name)}" +
-  #     "&api_key=#{ENV['LASTFM_KEY']}" +
-  #     "&format=json"
-  #   )
-  #   @lastfm_events = JSON.parse(url.body)
-
-  #   # Condtional to account for all different types of errors and instances coming from lastfm
-  #   if @lastfm_events['error'] == 6
-  #     # Do nothing?
-  #   elsif @lastfm_events['events']['total'] == '0'
-  #     # Do nothing?
-  #   elsif @lastfm_events['events']['@attr']['total'] == '1' 
-  #     venue = @lastfm_events['events']['event']['venue']
-  #     event = Event.where(url: @lastfm_events['events']['event']['url']).first
-  #     event = Event.create!(title: @lastfm_events['events']['event']['title'], venue: venue['name'], date: @lastfm_events['events']['event']['startDate'], url: @lastfm_events['events']['event']['url'], location: venue['postalcode'], lat: venue['location']['geo:point']['geo:lat'], long: venue['location']['geo:point']['geo:long'])
-  #     EventArtist.create!(artist: artist_name, event: event) unless EventArtist.where(artist: artist_name, event: event).first
-  #   else  
-  #     @lastfm_events['events']['event'].each do |events|
-  #       output = Hash.new
-  #       events.each do |key, value|
-  #         output[key] = value
-  #       end
-  #       output
-
-  #       event = Event.where(url: output['url']).first
-  #       event = Event.create!(title: output['title'], venue: output['venue']['name'], location: output['venue']['postalcode'], date: output['startDate'], url: output['url'], lat: output['venue']['location']['geo:point']['geo:lat'], long: output['venue']['location']['geo:point']['geo:long']) unless event
-        
-  #       EventArtist.create!(artist: artist_name, event: event) unless EventArtist.where(artist: artist_name, event: event).first
-  #     end
-  #   end
-
-  # end
-
 end
