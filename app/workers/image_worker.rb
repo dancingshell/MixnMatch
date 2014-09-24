041468c33450c935e5e41776fd288c96e15893c3
brandonkwong <brandonjk@gmail.com>
2014-09-24 03:25:54 -0700

    Updated image_worker to have non-existing artists on Last.fm to have default images, and updated Facebook user creation to redirect_to new_profile_path

--- app/workers/image_worker.rb
+++ app/workers/image_worker.rb
@@ -7,7 +7,7 @@ class ImageWorker
 
     @artist = Artist.find_by(id: artist_id)
     artist_name = @artist.name
-    artist_image = JSON.parse(
+    artist_url = JSON.parse(
       HTTParty.get(
         "http://ws.audioscrobbler.com/2.0/" +
         "?method=artist.getinfo" +
@@ -17,13 +17,16 @@ class ImageWorker
         "&api_key=2d4eafcd13fe4bd7da79ea43ecd3dded" +
         "&format=json"
       ).body
-    )['artist']['image'][-1]['#text']
+    )
 
-    if artist_image != ''
-      @artist.update(image: artist_image)
-    else
+    if artist_url['error'] == 6
       # Note: temporary URL until Heroku deployment of artist_default.png
       @artist.update(image: 'http://brandonkwong.com/ga/mixnmatch/artist_default.png')
+    elsif artist_url['artist']['image'][-1]['#text'] == ''
+      # Note: temporary URL until Heroku deployment of artist_default.png
+      @artist.update(image: 'http://brandonkwong.com/ga/mixnmatch/artist_default.png')
+    else
+      @artist.update(image: artist_url['artist']['image'][-1]['#text'])
     end
 
   end
