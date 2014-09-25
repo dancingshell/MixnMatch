ba5b4165e7880c5cc7afc583696d4b98574c1c4e
dancingshell <kaitlinjaffe@gmail.com>
2014-09-25 10:40:36 -0700

    checks presence of artists on match alogorythm

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -22,34 +22,36 @@ class MatchesController < ApplicationController
    
     @love_results = {}
     @final_results = Hash.new
-  
+
     @users.each do |u|
-      @user_artists = u.artists.map(&:name)
-      # finds artists that are common between current user and other users
-      @count = @user_artists & @current_user_artists
-      @profile = Profile.find_by(user_id: u.id)
-  
-      # secret match potion
-      if @current_user_artists.length > @user_artists.length
-        @large_count = @count.length.to_f / @current_user_artists.length
-        @small_count = @count.length.to_f / @user_artists.length
-      else
-        @large_count = @count.length.to_f / @user_artists.length
-        @small_count = @count.length.to_f / @current_user_artists.length
-      end
-      
-      @large_count = (@large_count + @small_count) / 2
-      @large_count *= 100
-      @small_count *= 100
-      @match_percent = (@large_count + @small_count) / 2 
+      if u.artists.length > 0
+        @user_artists = u.artists.map(&:name)
+        # finds artists that are common between current user and other users
+        @count = @user_artists & @current_user_artists
+        @profile = Profile.find_by(user_id: u.id)
+    
+        # secret match potion
+        if @current_user_artists.length > @user_artists.length
+          @large_count = @count.length.to_f / @current_user_artists.length
+          @small_count = @count.length.to_f / @user_artists.length
+        else
+          @large_count = @count.length.to_f / @user_artists.length
+          @small_count = @count.length.to_f / @current_user_artists.length
+        end
+        
+        @large_count = (@large_count + @small_count) / 2
+        @large_count *= 100
+        @small_count *= 100
+        @match_percent = (@large_count + @small_count) / 2 
 
-      # if number is devided by 0 (returns NaN) change to 0
-      @match_percent = 0 if @match_percent.nan?
+        # if number is devided by 0 (returns NaN) change to 0
+        @match_percent = 0 if @match_percent.nan?
 
-      # create hash of match% and profile info
-      @love_results[u] = [@match_percent.floor, @profile, u.profiles[0].age(u)]
-      # rank matches, highest first
-      @sorted_results = @love_results.sort_by{|k, v| v[0]}.reverse       
+        # create hash of match% and profile info
+        @love_results[u] = [@match_percent.floor, @profile, u.profiles[0].age(u)]
+        # rank matches, highest first
+        @sorted_results = @love_results.sort_by{|k, v| v[0]}.reverse  
+      end     
     end
     render json: @sorted_results
   end
